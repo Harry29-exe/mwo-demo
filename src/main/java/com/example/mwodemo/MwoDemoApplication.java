78405fdf474254be6dea8dd68efa2c302b3f4d82
Kamil Wójcik <wojcikakamil@gmail.com>
2022-01-08 12:05:48 +0100

    Initial commit

---
+++ src/main/java/com/example/mwodemo/MwoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mwodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwoDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mwodemo/MwoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mwodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
